1d7ab859d6c558fa93103ede51d783e6cd5a297c
Brittany Hill <brit.hill10@gmail.com>
2018-09-21 11:29:53 -0500

    Add tables to db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { db } = require('./models');
 
 app.use(morgan('dev'))
 app.use(express.static(__dirname + "/public"))
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -14,12 +14,21 @@ app.get('/', (req, res) => {
     res.send(main(''))
 })
 
-db.authenticate().
-then(() => {
-  console.log('connected to the database');
-})
+async function init() {
+    await db.sync({force: true})
+    await db.sync()
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`)
-})
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`App listening in port ${PORT}`)
+    })
+}
+
+init()
+
+db.authenticate().
+    then(() => {
+        console.log('connected to the database');
+    })
+
+
